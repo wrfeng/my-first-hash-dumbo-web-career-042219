0e5925337f3cc18bc4fff3890a8d7970dc05d3e6
wei feng <theweifeng@gmail.com>
2019-04-12 22:45:18 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  hash = {name => "nick halden"}
+  hash = {"name" => "nick halden"}
   
 end
 
